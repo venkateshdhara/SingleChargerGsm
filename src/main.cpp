dde64ab9ca6a46f734a6583ccc252134cc5857c6
Venatesh <venkateshvnk25@gmail.com>
2022-03-13 17:43:27 +0530

    added shorten json for post

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@
 #define currCalibration                                             8
 #define DHTTYPE                                                     DHT11
 #define DHTPIN                                                      12 
-#define RELAY                                                       10
+#define RELAY                                                       6
 #define START_BUTTON                                                4
 #define STOP_BUTTON                                                 5
 #define VADC                                                        A0
@@ -30,17 +30,20 @@ EnergyMonitor emon1;
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 /*--------------------------------------------------------VARIABLES-------------------------------------------------------------------------------------------*/
-const char BEARER[] PROGMEM = "airtelgprs.com";
-const char URL[] = "http://54.159.4.4:5000/000003/buttonstatus";
-const char URLP[] ="http://54.159.4.4:5000/000003/chargerhealth";
+const char BEARER_AIRTEL[] PROGMEM = "airtelgprs.com";
+const char BEARER_VI[] PROGMEM ="VI Net Speed";
+const char BEARER_BSNL[] PROGMEM ="bsnlnet";
+
+
+const char URL[] = "http://54.159.4.4:5000/000001/buttonstatus";
+const char URLP[] ="http://54.159.4.4:5000/000001/chargerhealth";
 const char CONTENT_TYPE[] = "application/json";
-char httpData[200];
-unsigned long lastmillis = 0,prevtime=0, postLastmill=0;
+char httpData[50];
+unsigned long lastmillis = 0,prevtime=0, startmillis=0, endmillis=0, sessionTime=0;
 bool timerRun = false, chargerState=false;            
-int addr = 0, rmode, level, pinData=1, blynkUseFlag, blynkBtn=2, prevState=2;
-float w, kWh,vMin, vMax, voltage;
-char response[32];
-char body[90];
+int  rmode, level, pinData=1, blynkUseFlag, blynkBtn=2, prevState=2;
+const int nwSelectEEPROMaddress=10, addr = 0;
+float w, kWh,vMin, vMax, voltage, temperature;
 Result result;
 HTTP http(9600, RX_PIN, TX_PIN, RST_PIN);
 
@@ -53,7 +56,8 @@ void chargerOn(void);
 void updateVA(void);
 void myTimerEvent(void);
 float readChamberTemperature(void);
-void postdata(float voltage, int level, int status, float acVoltage, float acCurrent, float kwh);
+void postdata(float voltage, int level, int status, float acVoltage, float acCurrent, float kwh, float temperature, int time, float units);
+void initPostOFF(void);
 /*-------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 void setup() {  
   pinMode(START_BUTTON, INPUT);
@@ -61,13 +65,52 @@ void setup() {
   pinMode(RELAY, OUTPUT);
   pinMode (FAN_RELAY,OUTPUT);
   dht.begin();
-  if(digitalRead(START_BUTTON) == 1){
-    EEPROM.write(addr,1);
+  lcd.init();
+  lcd.backlight();
+  if(digitalRead(START_BUTTON) == 1  &  digitalRead(STOP_BUTTON) == 1){
+    while(digitalRead(START_BUTTON) == 1  &  digitalRead(STOP_BUTTON) == 1);
+    while(digitalRead(STOP_BUTTON) == 0){
+    int selectionOfNetwork=EEPROM.read(nwSelectEEPROMaddress);
+    if(digitalRead(START_BUTTON) == 1){
+      selectionOfNetwork++;
+      if(selectionOfNetwork >3)
+         selectionOfNetwork=0;
+      EEPROM.write(nwSelectEEPROMaddress,selectionOfNetwork);  
+    if(selectionOfNetwork ==0){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Airtel selected");
+    blynkUseFlag =true;
+    EEPROM.write(addr, 1);
+    }
+    else if(selectionOfNetwork ==1){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("VI selected");
+    blynkUseFlag =true;
+    EEPROM.write(addr, 1);
+    }
+    else if(selectionOfNetwork ==2){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("BSNL selected");
+    blynkUseFlag =true;
+    EEPROM.write(addr, 1);
+    }
+    else if(selectionOfNetwork ==3){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Mannual ");
+    blynkUseFlag =false;
+    EEPROM.write(addr, 0);
+    }
+ 
+      while(digitalRead(START_BUTTON) == 1);
+     }
+    }
+     while(digitalRead(STOP_BUTTON) == 1);
   }
 
-  if(digitalRead(STOP_BUTTON) == 1){
-    EEPROM.write(addr,0);
-  }
   lcd.init();
   lcd.backlight();
   Serial.begin(115200);
@@ -83,31 +126,44 @@ void setup() {
   chargerOff();
   emon1.voltage(1, vCalibration, 1.7);    
   emon1.current(3, currCalibration);
-   blynkUseFlag=1;
   if(blynkUseFlag == true){
-    lcd.clear();
+  lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("Connecting GPRS...");
-  result = http.connect(BEARER);
+  int selectionOfNetwork=EEPROM.read(nwSelectEEPROMaddress);
+  if(selectionOfNetwork == 0){
+    lcd.setCursor(0,1);
+    lcd.print("AIRTEL NETWORK");
+     result = http.connect(BEARER_AIRTEL);
+  }
+  else if (selectionOfNetwork ==1)
+  {
+    lcd.setCursor(0,1);
+    lcd.print("VI NETWORK");
+     result = http.connect(BEARER_VI);
+  }
+  else if (selectionOfNetwork ==2)
+  {
+    lcd.setCursor(0,1);
+    lcd.print("BSNL NETWORK");
+    result = http.connect(BEARER_BSNL);
+  }
+  
+  delay(5000);
   Serial.print(F("HTTP connect: "));
   Serial.println(result);
-   lcd.clear();
+  lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("GPRS Connected");
 
 }
-  
+  initPostOFF();
   Serial.println("Void setup complete!!!");
  
 }
  
 void loop() {
-lcd.setCursor(0,1);
-lcd.print("V:");
-lcd.print(voltage,1);
-lcd.print("   L:");
-lcd.print(level);
- 
+
  if(blynkUseFlag == true){
  blynkBtn =getChargerStatus();
  Serial.print("Get val:");
@@ -121,7 +177,8 @@ lcd.print(level);
     blynkBtn =0;
  }
 
- readChamberTemperature();
+temperature = readChamberTemperature();
+temperature =0;
  int inpuValue = analogRead(VADC);
 
  if(inpuValue >= 390 & inpuValue <= 503 ){
@@ -138,40 +195,51 @@ lcd.print(level);
     vMin = 69.0; vMax = 84.0;
   voltage=mapf(inpuValue, 640, 800, vMin, vMax);
   }
+  else{
+    voltage =0;
+  }
   
   level=mapf(voltage, vMin, vMax, 0, 100);
   if(level < 0)
   level = 0;
+lcd.setCursor(0,1);
+lcd.print("                ");
+lcd.setCursor(0,1);
+lcd.print("V:");
+lcd.print(voltage,1);
+lcd.print("   L:");
+lcd.print(level);
 
 if(prevState != blynkBtn){
  if(blynkBtn == 1) {
    prevState =1;
-    blynkBtn=2;
     #if (DEBUG ==1)
     Serial.println("on");
     #endif
     chargerOn();
+    startmillis = millis();
     timerRun = true;
     prevtime=millis(); lastmillis=millis();
   }
     if(blynkBtn == 0){
       prevState =0;
-    blynkBtn=2;
     #if (DEBUG == 1)
   Serial.println("off");
   #endif
   chargerOff();
   timerRun = false;
   float power= kWh;
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Power consu:");
-  lcd.print(kWh,3);
+  // lcd.clear();
+  // lcd.setCursor(0,0);
+  // lcd.print("Power consu:");
+  endmillis = millis();
+  sessionTime = (endmillis-startmillis)/60000;
+  // lcd.print(kWh,3);
   kWh=0;
 
   prevtime=0; lastmillis=0;
-  delay(10000);
-  lcd.clear();
+  // delay(10000);
+  // lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("                ");
   lcd.setCursor(2,0);
@@ -179,8 +247,13 @@ if(prevState != blynkBtn){
 //while(digitalRead(STOP_BUTTON) == HIGH );
   }
 }
+if(((millis() - prevtime) >1000) & timerRun){
+myTimerEvent();
+prevtime=millis();
+}
+
 if(blynkUseFlag == true){
-postdata(voltage,level,chargerState,emon1.Vrms,emon1.Irms,kWh);
+postdata(voltage,level,chargerState,emon1.Vrms,emon1.Irms,kWh,temperature,sessionTime,(kWh/1000));
 }
 }
 
@@ -190,6 +263,7 @@ float mapf(float x, float in_min, float in_max, float out_min, float out_max) {
 }
 /*******************************************************************************************************************************************/
 int getChargerStatus(void){
+  char response[32];
  result = http.get(URL, response);
  memcpy(httpData,response,strlen(response));
   int switcher = atoi(&httpData[18]);
@@ -276,7 +350,7 @@ Serial.print("\ttim: ");
 Serial.print((millis() - lastmillis)/1000);
 Serial.println("s");
 #endif
-updateVA();
+// updateVA();
 
 lastmillis = millis();
 }
@@ -294,21 +368,38 @@ float readChamberTemperature(void){
   else if (t > 25 & t < 40){
     digitalWrite(FAN_RELAY,HIGH);
   }
+  if (t == NAN){
+    t=0;
+  }
+  else if (t == -NAN){
+    t=0;
+  }
+  {
+    /* code */
+  }
+  
   return t;
 }
 /*---------------------------------------------------------------------------------------------------------------------------------------------------------*/
-void postdata(float voltage, int level, int status, float acVoltage, float acCurrent, float kwh){
+void postdata(float voltage, int level, int status, float acVoltage, float acCurrent, float kwh, float temperature, int time, float units){
   char p[5];
   dtostrf(voltage,4,1,p);
-  char acvol[5];
-  dtostrf(acVoltage,4,1,acvol);
+ int acvol = (int)acVoltage;
     char accurr[5];
-  dtostrf(acVoltage,4,1,accurr);
+  dtostrf(acCurrent,4,1,accurr);
     char ackwh[5];
-  dtostrf(acVoltage,4,1,ackwh);
+  dtostrf(kwh,4,1,ackwh);
+
+  char chambtemperature[5];
+  dtostrf(temperature,4,1,chambtemperature);
+
+  char acunits[5];
+  dtostrf(units,4,1,acunits);
   if(status >= 1)
     status=1;
- sprintf(body, "{\"level\": %d, \"voltage\": %s, \"status\":%d }", level, p,status);
+    char body[120];
+    char response[120];
+  sprintf(body, "{\"lvl\": %d, \"dcv\": %s, \"stat\":%d, \"acv\":%d, \"aci\":%s, \"kwh\":%s, \"time\":%d, \"t\":%s }", level, p,status,acvol,accurr,ackwh,time,chambtemperature);
   result = http.post(URLP, body, response);
   Serial.println(body);
   Serial.print(F("HTTP POST: "));
@@ -320,3 +411,8 @@ void postdata(float voltage, int level, int status, float acVoltage, float acCur
   }
 }
 /*---------------------------------------------------------------------------------------------------------------------------------------------------------*/
+void initPostOFF(void){
+  char msg[]="{\"charger_switch\":0}";
+  char response[30];
+  http.post(URL, msg, response);
+}
